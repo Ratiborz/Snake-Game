7d448f203e6a7d204479759416af0a2324329c68
Heorhi Zaturkevich <konisgung@gmail.com>
2023-07-01 18:44:54 +0300

    create data X-Y

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const board = document.querySelector('#board'); // Доска 
 const SQUARES_NUMBER = 702; // Количество клеток 
+const BOARD_WIDTH = 26; // Ширина доски в клетках
 
 for (let i = 0; i < SQUARES_NUMBER; i++) { // Рисует установленное количество клеток 
     const square = document.createElement('div');
     square.classList.add('square');
+
+    const x = i % BOARD_WIDTH; // Рассчет X координаты
+    const y = Math.floor(i / BOARD_WIDTH); // Рассчет Y координаты
+    
+    square.setAttribute('data-x', x);
+    square.setAttribute('data-y', y);
 
     board.append(square);
 }; 
